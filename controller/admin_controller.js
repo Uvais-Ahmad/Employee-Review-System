34c1b637490bc3818211b96e797b06c3404f4252
Uvais Ahmad <uvss21@gmail.com>
2022-11-15 23:53:31 +0530

    Showing list of all employees on employees page

--- controller/admin_controller.js
+++ controller/admin_controller.js
@@ -1,25 +1,37 @@
+const User = require('../models/users');
+
 module.exports.assignWork = function( req , res ){
 
-    if(req.user.isAdmin){
-        return res.render('_assignWork',{
-            title : "Assign Work"
-        });
-    }
-    else{
-        return res.redirect('/');
-    }
+    // find all employees
+    User.find({} , function(err , users){
+        if(err){ console.log('Error occur while finding employes');}
 
-    
+        //check if user is ADMIN
+        if(req.user.isAdmin){
+            return res.render('_assignWork',{
+                title : "Assign Work",
+                users : users
+            });
+        }
+        else{
+            return res.redirect('/');
+        }
+    });    
 }
 
 module.exports.allEmployee = function( req , res ){
 
-    if(req.user.isAdmin){
-        return res.render('_allEmployee',{
-            title : "Employees"
-        });
-    }
-    else{
-        return res.redirect('/');
-    }
+    User.find( {} , function(err , users ){
+        if(err){ console.log('Error occur while finding employes');}
+
+        if(req.user.isAdmin){
+            return res.render('_allEmployee',{
+                title : "Employees",
+                users : users
+            });
+        }
+        else{
+            return res.redirect('/');
+        }
+    })
 }
