04297db775b28cb92393bc4172a2e9c9f9ea4b5a
Uvais Ahmad <uvss21@gmail.com>
2022-11-27 20:35:15 +0530

    Heroku deploy

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,24 +1,24 @@
-const dotenv = require('dotenv').config();
+// const dotenv = require('dotenv').config();
 
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URL);
-console.log("mongoose connection");
-const db = mongoose.connection;
+// mongoose.connect(process.env.MONGODB_URL);
+// console.log("mongoose connection");
+// const db = mongoose.connection;
 
-db.on("error",console.error.bind(console,"Error occur while making mongoose connection"));
+// db.on("error",console.error.bind(console,"Error occur while making mongoose connection"));
 
-db.once('open',function(){
-    console.log("Connection success to mongoose .");
-});
+// db.once('open',function(){
+//     console.log("Connection success to mongoose .");
+// });
 
-module.exports = db;
+// module.exports = db;
 
 
 // Import the mongoose module
-// const mongoose = require('mongoose');
-// const dotenv = require('dotenv').config();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
 
-// //Set up default mongoose connection
-// var mongoDB = process.env.MONGODB_URL;
-// module.exports = mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log("CONNECTION ESTABLISHED"));
+//Set up default mongoose connection
+var mongoDB = process.env.MONGODB_URL;
+module.exports = mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log("CONNECTION ESTABLISHED"));
